653a35a6a40f9f831499ab09dd21704b9c5759aa
Celina Rabe <celinarabe@yahoo.com>
2021-04-09 09:08:45 -0500

    integrated feedback

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import './App.css';
+import "./App.css";
 import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Routes from './components/Routes'
-import Nav from './components/Navigation'
+import Routes from "./components/Routes";
+import Nav from "./components/Navigation";
 import "./App.css";
 
-
 function App() {
-  return (
-    <div>
-
-      <Routes />
-      
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
@@ -8,12 +8,11 @@ const Home = () => {
     <div>
       <Container fluid className="center">
         <Row className="justify-content-center align-self-center">
-        <h1>Welcome to our skincare store</h1>
+          <h1>Welcome to our skincare store</h1>
         </Row>
       </Container>
-      
     </div>
-  )
-}
+  );
+};
 
 export default Home;
